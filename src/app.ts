d292259e43e31129c68d39a9d83084845f4fef30
Aleksandr Prikhodko <kakto228030904@gmail.com>
2026-09-19 20:18:09 +0300

    fix: add catch-all 404 handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { NotFoundError } from './errors/error.ts';
 import {
 	contextMiddleware,
 	errorHandler,
@@ -14,6 +15,9 @@ app.use(express.json({ limit: '100kb' }));
 
 app.use('/api', healthRouter);
 app.use('/api', equipmentRouter);
+app.use('/', () => {
+	throw new NotFoundError('Маршрут');
+});
 
 app.use(errorHandler);
 
